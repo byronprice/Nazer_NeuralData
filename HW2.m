d37c9feb7661cff7a7d144d7219ee52d35c9fe58
byronprice <byron.h.price@gmail.com>
2017-11-29 22:14:30 -0500

    Correct calculations of AUC for ROC

--- HW2.m
+++ HW2.m
@@ -1,15 +1,15 @@
 % HW2.m
 %  HW2 for Nazer's neural data class ... network inference from calcium
 %   imaging data
-myCluster = parcluster('local');
+% myCluster = parcluster('local');
+% 
+% if getenv('ENVIRONMENT')
+%    myCluster.JobStorageLocation = getenv('TMPDIR'); 
+% end
+% 
+% parpool(myCluster,2);
 
-if getenv('ENVIRONMENT')
-   myCluster.JobStorageLocation = getenv('TMPDIR'); 
-end
-
-parpool(myCluster,6);
-
-load('small_net1-Poisson.mat');
+load('small_net1-spikes.mat');
 
 reduceData = spikeTrains;
 [N,numNeurons] = size(reduceData);
@@ -18,17 +18,16 @@ connectivityMatrix1 = zeros(numNeurons,numNeurons);
 connectivityMatrix2 = zeros(numNeurons,numNeurons);
 histParams = 25;
 allInds = 1:numNeurons;
-parfor ii=1:numNeurons
+for ii=1:numNeurons
     Y = reduceData(histParams+1:end,ii);
     H = zeros(length(Y),histParams);
     for jj=1:histParams
        H(:,jj) = reduceData(histParams+1-jj:end-jj,ii); 
     end
-
-    inds = find(allInds~=ii);
-    Design = [ones(length(Y),1),H,reduceData(histParams+1:end,inds),reduceData(histParams:end-1,inds)];
+    tic;
+    Design = [ones(length(Y),1),H,reduceData(histParams+1:end,allInds~=ii)];
     [~,fullDev,~] = glmfit(Design,Y,'poisson','constant','off');
-
+    toc;
     
     tempConn1 = zeros(1,numNeurons);
     tempConn2 = zeros(1,numNeurons);
@@ -36,7 +35,7 @@ parfor ii=1:numNeurons
         tempConn1(jj) = fullDev;
         
         newInds = find(allInds~=ii & allInds~=jj);
-        Design = [ones(length(Y),1),H,reduceData(histParams+1:end,newInds),reduceData(histParams:end-1,inds)];
+        Design = [ones(length(Y),1),H,reduceData(histParams+1:end,newInds)];
         [~,dev,~] = glmfit(Design,Y,'poisson','constant','off');
         tempConn2(jj) = dev;
     end
@@ -45,12 +44,12 @@ parfor ii=1:numNeurons
     fprintf('Done with neuron %d\n',ii);
 end
 
-save('SmallNet1_PoissonResults.mat','connectivityMatrix1','connectivityMatrix2',...
+save('SmallNet1_OASISResults.mat','connectivityMatrix1','connectivityMatrix2',...
     'numNeurons','N','histParams');
 
 delete(gcp);
 
-load('SmallNet1_PoissonResults.mat');
+load('SmallNet1_OASISResults.mat');
 load('network_small-net1.mat');
 
 connectMat = zeros(numNeurons,numNeurons);
@@ -78,10 +77,12 @@ end
 % get p-vals for each connection, then do Benjamini-Hochberg procedure with
 %  p-vals in hand
 PVALmat = zeros(numNeurons,numNeurons);
-fullParams = (numNeurons-1)*2+histParams+1;
-restrictParams = (numNeurons-2)*2+histParams+1;
+fullParams = numNeurons-1+histParams+1;
+restrictParams = numNeurons-2+histParams+1;
 paramDiff = fullParams-restrictParams;
 allInds = 1:numNeurons;
+
+devDiff = zeros(numNeurons,numNeurons);
 for ii=1:numNeurons
     inds = find(allInds~=ii);
     for jj=inds
@@ -89,6 +90,7 @@ for ii=1:numNeurons
         devRestrict = connectivityMatrix2(ii,jj);
         chiSquare = devRestrict-devFull;
         temp = chi2cdf(chiSquare,paramDiff,'upper');
+        devDiff(ii,jj) = chiSquare;
         if temp<1e-50
             temp = 1e-50;
         end
@@ -103,7 +105,8 @@ truePositives = zeros(numTests,1);
 falsePositives = zeros(numTests,1);
 accuracy = zeros(numTests,1);
 
-vectorPvals = sort(PVALmat(PVALmat~=0));
+inds = ~diag(ones(numNeurons,1));
+vectorPvals = sort(PVALmat(inds));
 m = length(vectorPvals);
 for ii=1:numTests
     % Benjamini-Hochberg
@@ -118,7 +121,7 @@ for ii=1:numTests
     end
     
     % threshold to get directed connectivity
-    newPmat = PVALmat;newPmat(newPmat>pThreshold) = 0;
+    newPmat = PVALmat;newPmat(newPmat>=pThreshold) = 0;
     newPmat(newPmat>0) = 1;
     
     
@@ -131,23 +134,27 @@ for ii=1:numTests
              end
         end
     end
+    onesMatrix = ones(size(undirPmat));
+    inds = find(triu(onesMatrix,1));
+    undirPvec = undirPmat(inds);
+    undirConnVec = undirConnectMat(inds);
     
     % calculate error, etc.
-    temp = undirPmat+undirConnectMat;
-    accuracy(ii) = (sum(sum(temp==2))+sum(sum(temp==0)))/(numNeurons*numNeurons);
+    temp = undirPvec+undirConnVec;
+    accuracy(ii) = (sum(sum(temp==2))+sum(sum(temp==0)))/(numNeurons*numNeurons/2-numNeurons);
     
     % get true positive rate
-    truePositives(ii) = sum(sum(temp==2))/sum(sum(undirConnectMat==1));
+    truePositives(ii) = sum(sum(temp==2))/sum(sum(undirConnVec==1));
     % get false positive rate
-    negatives = find(undirConnectMat==0);
-    guessPositives = find(undirPmat==1);
+    negatives = find(undirConnVec==0);
+    guessPositives = find(undirPvec==1);
     falsePositives(ii) = length(intersect(negatives,guessPositives))/length(negatives);
 end
 
 figure;
 plot(falsePositives,truePositives,'b','LineWidth',2);hold on;
 plot(linspace(0,1,numTests),linspace(0,1,numTests),'--k');
-title(sprintf('ROC Curve'));
+title(sprintf('ROC Curve: Granger Causality Raw Traces'));
 xlabel('False Positive Rate');ylabel('True Positive Rate');
 legend('Granger F-test','Null','Location','SouthEast');
 
@@ -156,3 +163,28 @@ fprintf('AUROCC: %3.2f\n',AUC);
 
 [maxAc,ind] = max(accuracy);
 fprintf('Maximum Accuracy at FDR %3.2e: %3.2f\n',FDR(ind),maxAc);
+
+onesMatrix = ones(numNeurons,numNeurons);
+inds = find(triu(onesMatrix,1));
+temp = undirConnectMat(inds);
+
+for ii=1:numNeurons
+   for jj=1:numNeurons
+       temp1 = devDiff(ii,jj);
+       temp2 = devDiff(jj,ii);
+       if temp2>temp1
+           devDiff(ii,jj) = temp2;
+       end
+   end
+end
+
+devDiff = devDiff(inds);
+devDiff1 = devDiff1(temp==1);
+devDiff0 = devDiff0(temp==0);
+
+figure;histogram(devDiff1,'normalization','probability');hold on;
+histogram(devDiff0,'normalization','probability');
+legend('Connection','No Connection','Location','Northwest');
+title('Histogram of Chi-Square statistics');
+xlabel('Chi-Square Statistic');
+ylabel('Count');
