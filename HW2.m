30ee8883b38de30d9e3cdc9347889312b423df63
Byron Price <byron.h.price@gmail.com>
2017-12-02 17:44:49 -0500

    try sparse representation

--- HW2.m
+++ HW2.m
@@ -4,18 +4,19 @@
 myCluster = parcluster('local');
 
 if getenv('ENVIRONMENT')
-   myCluster.JobStorageLocation = getenv('TMPDIR'); 
+   myCluster.JobStorageLocation = getenv('TMPDIR');
 end
 
 parpool(myCluster,6);
 
 load('small_net1-spikes.mat');
-
-reduceData = spikeTrains(10001:60000,:);
+reduceData = spikeTrains;
 [N,numNeurons] = size(reduceData);
 
 connectivityMatrix1 = zeros(numNeurons,numNeurons);
 connectivityMatrix2 = zeros(numNeurons,numNeurons);
+parameterMatrix1 = zeros(numNeurons,numNeurons);
+parameterMatrix2 = zeros(numNeurons,numNeurons);
 histParams = 50;numBases = 10;
 basisFuns = zeros(histParams,numBases);
 for ii=1:numBases
@@ -29,31 +30,61 @@ parfor ii=1:numNeurons
     for jj=1:histParams
        H(:,jj) = reduceData(histParams+1-jj:end-jj,ii); 
     end
+    
+    forDesign = [reduceData(histParams+1:end,allInds~=ii),reduceData(histParams:end-1,allInds~=ii)];
+    Design = [ones(length(Y),1),H*basisFuns,forDesign];
 
-    Design = [ones(length(Y),1),H*basisFuns,reduceData(histParams+1:end,allInds~=ii)];
-    [~,fullDev,~] = glmfit(Design,Y,'poisson','constant','off');
+    b = Design\Y;
+    inds = numBases+2:length(b);
+    temp = b(inds);
+    sigma = 1.4826.*mad(temp,1);
+    newInds = abs(temp)<sigma;
+    forDesign(:,newInds) = [];
+    
+    Design = [ones(length(Y),1),H*basisFuns,forDesign];
+    b = Design\Y;
+    fullDev = sum((Design*b-Y).^2);
+    params1 = length(b);
     
     tempConn1 = zeros(1,numNeurons);
     tempConn2 = zeros(1,numNeurons);
+    tempParams1 = zeros(1,numNeurons);
+    tempParams2 = zeros(1,numNeurons);
     for jj=1:numNeurons
         tempConn1(jj) = fullDev;
+        tempParams1(jj) = params1;
         
         newInds = find(allInds~=ii & allInds~=jj);
-        Design = [ones(length(Y),1),H*basisFuns,reduceData(histParams+1:end,newInds)];
-        [~,dev,~] = glmfit(Design,Y,'poisson','constant','off');
+        forDesign = [reduceData(histParams+1:end,newInds),reduceData(histParams:end-1,newInds)];
+        Design = [ones(length(Y),1),H*basisFuns,forDesign];
+      
+        b = Design\Y;
+        inds = numBases+2:length(b);
+        temp = b(inds);
+        sigma = 1.4826.*mad(temp,1);
+        newInds = abs(temp)<sigma;
+        forDesign(:,newInds) = [];
+        
+        Design = [ones(length(Y),1),H*basisFuns,forDesign];
+        b = Design\Y;
+        dev = sum((Design*b-Y).^2);
+        tempParams2(jj) = length(b);
+
         tempConn2(jj) = dev;
     end
     connectivityMatrix1(ii,:) = tempConn1;
     connectivityMatrix2(ii,:) = tempConn2;
+    parameterMatrix1(ii,:) = tempParams1;
+    parameterMatrix2(ii,:) = tempParams2;
     fprintf('Done with neuron %d\n',ii);
 end
 
-save('SmallNet1_OASISResults.mat','connectivityMatrix1','connectivityMatrix2',...
-    'numNeurons','N','histParams','numBases');
+save('SmallNet1_OASISResults-Lasso.mat','connectivityMatrix1','connectivityMatrix2',...
+    'numNeurons','N','histParams','numBases','parameterMatrix1','parameterMatrix2');
 
 delete(gcp);
 
-load('SmallNet1_OASISResults.mat');
+load('SmallNet1_OASISResults-Lasso.mat');
 load('network_small-net1.mat');
 
 connectMat = zeros(numNeurons,numNeurons);
@@ -81,15 +112,19 @@ end
 % get p-vals for each connection, then do Benjamini-Hochberg procedure with
 %  p-vals in hand
 PVALmat = zeros(numNeurons,numNeurons);
-fullParams = numNeurons-1+numBases+1;
-restrictParams = numNeurons-2+numBases+1;
-paramDiff = fullParams-restrictParams;
+% fullParams = (numNeurons-1)*2+numBases+1;
+% restrictParams = (numNeurons-2)*2+numBases+1;
+% paramDiff = fullParams-restrictParams;
 allInds = 1:numNeurons;
 
 devDiff = zeros(numNeurons,numNeurons);
 for ii=1:numNeurons
     inds = find(allInds~=ii);
     for jj=inds
+        fullParams = parameterMatrix1(ii,jj);
+        restrictParams = parameterMatrix2(ii,jj);
+        paramDiff = max(fullParams-restrictParams,1);
+        
         devFull = connectivityMatrix1(ii,jj);
         devRestrict = connectivityMatrix2(ii,jj);
         chiSquare = devRestrict-devFull;
@@ -183,8 +218,8 @@ for ii=1:numNeurons
 end
 
 devDiff = devDiff(inds);
-devDiff1 = devDiff1(temp==1);
-devDiff0 = devDiff0(temp==0);
+devDiff1 = devDiff(temp==1);
+devDiff0 = devDiff(temp==0);
 
 figure;histogram(devDiff1,'normalization','probability');hold on;
 histogram(devDiff0,'normalization','probability');
