e6089357e54c68d0f892502f229847dc35d0b588
byronprice <byron.h.price@gmail.com>
2017-12-03 22:09:44 -0500

    Test stimulus transformations

---
+++ NaturalImageTest.m
@@ -0,0 +1,308 @@
+function [] = NaturalImageTest()
+% NaturalImageTest.m
+
+cd ~/CloudStation/ByronExp/RestructuredNaturalImages/
+
+load('NaturalImages.mat','allIms');
+load('ResNaturalImages.mat','resIms');
+
+allIms = double(allIms);resIms = double(resIms);
+allIms = (allIms-mean(allIms(:)))/std(allIms(:));
+resIms = (resIms-mean(resIms(:)))/std(resIms(:));
+
+N = 500;DIM = [200,200];
+
+allIms = allIms(:,:,1:N);
+resIms = resIms(:,:,1:N);
+
+[X,Y] = meshgrid(1:DIM(2),1:DIM(1));
+
+filter = @(x) x(3).*exp(-(X-100).*(X-100)/(2*10*10)-(Y-100).*(Y-100)/(2*15*15))...
+    .*sin(2*pi.*(cos(x(2)-pi/2).*X+sin(x(2)-pi/2).*Y).*x(1));
+
+mask = zeros(DIM);
+for ii=1:DIM(1)
+    for jj=1:DIM(2)
+        dist = sqrt((ii-100).^2+(jj-100).^2);
+        if dist<30
+            mask(ii,jj) = 1;
+        end
+    end
+end
+maskInds = find(mask);
+
+imrotations = [0,90,180,270];
+
+a = 2;b = 0.035;c = 50;
+sigmoid = @(x,a,b,c) a./(1+exp(-b.*(x-c)));
+
+imrotations = [0,90,180,270];
+spatialFrequencies = [1/100,1/75,1/50,1/40,1/30,1/20,1/10,1/5];
+minLens = 5.*ones(length(spatialFrequencies),1);
+
+resultNames = {'ASD_Nat','ASD_PoissNat','ASD_ResNat','ASD_PoissResNat',...
+    'ASD_White','ASD_PoissWhite','ASD_Pink','ASD_PoissPink',...
+    'STA_Nat','STA_PoissNat','STA_ResNat','STA_PoissResNat','STA_White',...
+    'STA_PoissWhite','STA_Pink','STA_PoissPink'};
+
+numConditions = length(resultNames);
+
+biasOrient = zeros(length(spatialFrequencies),numConditions,2);
+biasSpatFreq = zeros(length(spatialFrequencies),numConditions,2);
+
+mseOrient = zeros(length(spatialFrequencies),numConditions,2);
+mseSpatFreq = zeros(length(spatialFrequencies),numConditions,2);
+mseFull = zeros(length(spatialFrequencies),numConditions,2);
+
+numIter = 1000;
+
+for jj=1:length(spatialFrequencies)
+    spatFreq = spatialFrequencies(jj);
+    minLen = minLens(jj);
+    resultsOrient = zeros(numIter,numConditions);
+    resultsSpatFreq = zeros(numIter,numConditions);
+    resultsFull = zeros(numIter,numConditions);
+    for kk=1:numIter
+        
+        % SIMULATE DATA
+        orientation = rand*2*pi;
+        currentFilter = filter([spatFreq,orientation,1]);
+        filterNatOutput = zeros(N,1);
+        filterResNatOutput = zeros(N,1);
+        filterWhiteOutput = zeros(N,1);
+        filterPinkOutput = zeros(N,1);
+        
+        naturalData = zeros(N,DIM(1)*DIM(2));
+        resNatData = zeros(N,DIM(1)*DIM(2));
+        whiteData = normrnd(0,1,[N,DIM(1)*DIM(2)]);
+        pinkData = zeros(N,DIM(1)*DIM(2));
+        for ii=1:N
+            rotate = imrotations(randperm(4,1));
+            temp1 = allIms(100:DIM(1)+99,100:DIM(2)+99,ii);
+            temp2 = resIms(100:DIM(1)+99,100:DIM(2)+99,ii);
+            temp1 = imrotate(temp1,rotate);
+            temp2 = imrotate(temp2,rotate);
+            temp3 = reshape(whiteData(ii,:),DIM);
+            temp4 = spatialPattern(DIM,-2);
+            temp4 = (temp4-mean(temp4(:)))/std(temp4(:));
+            
+            naturalData(ii,:) = temp1(:);
+            resNatData(ii,:) = temp2(:);
+            pinkData(ii,:) = temp4(:);
+            
+            filterNatOutput(ii) = sum(sum(temp1.*currentFilter));
+            filterResNatOutput(ii) = sum(sum(temp2.*currentFilter));
+            filterWhiteOutput(ii) = sum(sum(temp3.*currentFilter));
+            filterPinkOutput(ii) = sum(sum(temp4.*currentFilter));
+        end
+        
+        filterNatOutput = filterNatOutput+normrnd(0,std(filterNatOutput)/5,[N,1]);
+        filterResNatOutput = filterResNatOutput+normrnd(0,std(filterResNatOutput)/5,[N,1]);
+        filterWhiteOutput = filterWhiteOutput+normrnd(0,std(filterWhiteOutput)/5,[N,1]);
+        filterPinkOutput = filterPinkOutput+normrnd(0,std(filterPinkOutput)/5,[N,1]);
+        
+        filterNatOutput = filterNatOutput-mean(filterNatOutput);
+        filterResNatOutput = filterResNatOutput-mean(filterResNatOutput);
+        filterWhiteOutput = filterWhiteOutput-mean(filterWhiteOutput);
+        filterPinkOutput = filterPinkOutput-mean(filterPinkOutput);
+        
+        poissNat = filterNatOutput;
+        poissResNat = filterResNatOutput;
+        poissWhite = filterWhiteOutput;
+        poissPink = filterPinkOutput;
+        
+        poissNat = poissrnd(sigmoid(poissNat,a,b,c));
+        poissNat = poissNat-mean(poissNat);
+        poissResNat = poissrnd(sigmoid(poissResNat,a,b,c));
+        poissResNat = poissResNat-mean(poissResNat);
+        poissWhite = poissrnd(sigmoid(poissWhite,a,b,c));
+        poissWhite = poissWhite-mean(poissWhite);
+        poissPink = poissrnd(sigmoid(poissPink,a,b,c));
+        poissPink = poissPink-mean(poissPink);
+        
+        
+        % GET ESTIMATED FILTERS
+        
+        % natural images with asd
+        [natFilt,~] = fastASD(naturalData,filterNatOutput,DIM,minLen);
+        [poissNatFilt,~] = fastASD(naturalData,poissNat,DIM,minLen);
+        
+        % restructured natural images with asd
+        [resNatFilt,~] = fastASD(resNatData,filterResNatOutput,DIM,minLen);
+        [poissResNatFilt,~] = fastASD(resNatData,poissResNat,DIM,minLen);
+        
+        % white noise with asd
+        [whiteFilt,~] = fastASD(whiteData,filterWhiteOutput,DIM,minLen);
+        [poissWhiteFilt,~] = fastASD(whiteData,poissWhite,DIM,minLen);
+        
+        % pink noise with asd
+        [pinkFilt,~] = fastASD(pinkData,filterPinkOutput,DIM,minLen);
+        [poissPinkFilt,~] = fastASD(pinkData,poissPink,DIM,minLen);
+        
+        % natural images with sta
+        staNat = naturalData'*filterNatOutput;
+        b = staNat\currentFilter(:);
+        staNat = staNat*b;
+        staPoissNat = naturalData'*poissNat;
+        b = staPoissNat\currentFilter(:);
+        staPoissNat = staPoissNat*b;
+        
+        % restructured natural images with sta
+        staResNat = resNatData'*filterResNatOutput;
+        b = staResNat\currentFilter(:);
+        staResNat = staResNat*b;
+        staPoissResNat = resNatData'*poissResNat;
+        b = staPoissResNat\currentFilter(:);
+        staPoissResNat = staPoissResNat*b;
+        
+        % white noise with sta
+        staWhite = whiteData'*filterWhiteOutput;
+        b = staWhite\currentFilter(:);
+        staWhite = staWhite*b;
+        staPoissWhite = whiteData'*poissWhite;
+        b = staPoissWhite\currentFilter(:);
+        staPoissWhite = staPoissWhite*b;
+        
+        % pink noise with sta
+        staPink = pinkData'*filterPinkOutput;
+        b = staPink\currentFilter(:);
+        staPink = staPink*b;
+        staPoissPink = pinkData'*poissPink;
+        b = staPoissPink\currentFilter(:);
+        staPoissPink = staPoissPink*b;
+        
+        
+        % FIT GABOR FILTERS
+        
+        % natural images with asd
+        temp = reshape(natFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,1) = x(1)-spatFreq;
+        resultsOrient(kk,1) = x(2)-orientation;
+        resultsFull(kk,1) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(poissNatFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,2) = x(1)-spatFreq;
+        resultsOrient(kk,2) = x(2)-orientation;
+        resultsFull(kk,2) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % restructured natural images with asd
+        temp = reshape(resNatFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,3) = x(1)-spatFreq;
+        resultsOrient(kk,3) = x(2)-orientation;
+        resultsFull(kk,3) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(poissResNatFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,4) = x(1)-spatFreq;
+        resultsOrient(kk,4) = x(2)-orientation;
+        resultsFull(kk,4) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % white noise with asd
+        temp = reshape(whiteFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,5) = x(1)-spatFreq;
+        resultsOrient(kk,5) = x(2)-orientation;
+        resultsFull(kk,5) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(poissWhiteFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,6) = x(1)-spatFreq;
+        resultsOrient(kk,6) = x(2)-orientation;
+        resultsFull(kk,6) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % pink noise with asd
+        temp = reshape(pinkFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,7) = x(1)-spatFreq;
+        resultsOrient(kk,7) = x(2)-orientation;
+        resultsFull(kk,7) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(poissPinkFilt,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,8) = x(1)-spatFreq;
+        resultsOrient(kk,8) = x(2)-orientation;
+        resultsFull(kk,8) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % natural images with sta
+        temp = reshape(staNat,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,9) = x(1)-spatFreq;
+        resultsOrient(kk,9) = x(2)-orientation;
+        resultsFull(kk,9) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(staPoissNat,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,10) = x(1)-spatFreq;
+        resultsOrient(kk,10) = x(2)-orientation;
+        resultsFull(kk,10) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % restructured natural images with sta
+        temp = reshape(staResNat,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,11) = x(1)-spatFreq;
+        resultsOrient(kk,11) = x(2)-orientation;
+        resultsFull(kk,11) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(staPoissResNat,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,12) = x(1)-spatFreq;
+        resultsOrient(kk,12) = x(2)-orientation;
+        resultsFull(kk,12) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % white noise with sta
+        temp = reshape(staWhite,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,13) = x(1)-spatFreq;
+        resultsOrient(kk,13) = x(2)-orientation;
+        resultsFull(kk,13) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(staPoissWhite,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,14) = x(1)-spatFreq;
+        resultsOrient(kk,14) = x(2)-orientation;
+        resultsFull(kk,14) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        % pink noise with sta
+        temp = reshape(staPink,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,15) = x(1)-spatFreq;
+        resultsOrient(kk,15) = x(2)-orientation;
+        resultsFull(kk,15) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+        
+        temp = reshape(staPoissPink,DIM);
+        x = FitFilter(temp,[spatFreq,orientation,1],X,Y);
+        resultsSpatFreq(kk,16) = x(1)-spatFreq;
+        resultsOrient(kk,16) = x(2)-orientation;
+        resultsFull(kk,16) = mean(abs(temp(maskInds)-currentFilter(maskInds)));
+    end
+    
+    for kk=1:numConditions
+        biasSpatFreq(jj,kk,1) = mean(resultsSpatFreq(:,kk));
+        biasOrient(jj,kk,1) = mean(resultsOrient(:,kk));
+        biasSpatFreq(jj,kk,2) = std(resultsSpatFreq(:,kk));
+        biasOrient(jj,kk,2) = std(resultsOrient(:,kk));
+        
+        mseSpatFreq(jj,kk,1) = mean(resultsSpatFreq(:,kk).^2);
+        mseOrient(jj,kk,1) = mean(resultsOrient(:,kk).^2);
+        mseFull(jj,kk,1) = mean(resultsFull(:,kk).^2);
+        mseSpatFreq(jj,kk,2) = std(resultsSpatFreq(:,kk).^2);
+        mseOrient(jj,kk,2) = std(resultsOrient(:,kk).^2);
+        mseFull(jj,kk,2) = std(resultsFull(:,kk).^2);
+    end
+end
+
+save Natural_Res_Ims-Test.mat mseSpatFreq mseOrient mseFull biasSpatFreq biasOrient ...
+    resultNames spatialFrequencies DIM filter numConditions;
+end
+
+function [x] = FitFilter(y,x0,X,Y)
+lb = [0,0,-Inf];ub = [Inf,Inf,Inf];
+myFun = @(x) x(3).*exp(-(X-100).*(X-100)/(2*10*10)-(Y-100).*(Y-100)/(2*15*15))...
+    .*sin(2*pi.*(cos(x(2)-pi/2).*X+sin(x(2)-pi/2).*Y).*x(1))-y;
+
+x = lsqnonlin(myFun,x0,lb,ub);
+
+end
