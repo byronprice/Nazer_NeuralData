6ec14fa8ee5795bdf7221fbde05e41577d31c6b2
Byron Price <byron.h.price@gmail.com>
2017-12-21 14:25:08 -0500

    ASD versus DCT PCA on natural images

--- TestNatIms.m
+++ TestNatIms.m
@@ -8,7 +8,7 @@ load('NaturalImages.mat');
 imMean = mean(double(allIms),3);
 
 N = 750;DIM = [200,200];
-dctDIM = 100*100;
+dctDIM = 125*125;
 fullData = zeros(N,prod(DIM));
 dctData = zeros(N,dctDIM);
 for ii=1:750
@@ -17,52 +17,48 @@ for ii=1:750
     fullData(ii,:) = double(temp(:));
 
     R = mirt_dctn(temp);
-    R = R(1:100,1:100);
+    R = R(1:125,1:125);
     dctData(ii,:) = R(:);
 end
+
+clear allIms;
 
 % consider first getting rid of a bunch of values and then doing PCA
 %  on data that has been Z-scored or on data that has had its 
 %  mean divided out  
 
-% S = cov(dctData); % or try shrinkage_cov
-S = shrinkage_cov(dctData);
+S = cov(dctData); % or try shrinkage_cov
+%S = shrinkage_cov(dctData);
 [V,D] = eig(S);
 
-% choose dimensionality for dct images
-allEigs = diag(D);
-fullVariance = sum(allEigs);
-for ii=10:10:dctDim
-    start = dctDim-ii+1;
-    eigenvals = allEigs(start:end);
-    varianceProp = sum(eigenvals)/sum(fullVariance);
-    if varianceProp <= 0.95
-        break;
-    end
-end
-q = length(allEigs(start:end));
-meanEig = mean(allEigs(1:start-1));
-W = V(:,start:end)*sqrtm(D(start:end,start:end)-meanEig.*eye(q));
-W = fliplr(W);
+pcaData = dctData';
+mu = mean(pcaData,2);
+pcaData = pcaData-repmat(mu,[1,N]);
 
-dctData = dctData';
-mu = mean(dctData,2);
-dctData = dctData-repmat(mu,[1,N]);
-x = pinv(W)*dctData;
 
-reduceDctData = x'; % N-by-however-many-dimensions are kept
-Sdct = cov(reduceDctData);
-
-lPath = [10,5,4,3,2,1.5,1,0.75,0.5,0.25,0.1,0.05,0.02,0.01,0.001];
-[XP,~,~,~,~,~] = QUIC('path',Sdct,50,lPath, 1e-8, 2, 100);
-
-newXP = zeros(q,q,length(lPath)+1);
-newXP(:,:,1:length(lPath)) = XP;
-newXP(:,:,end) = (N-1).*pinv(reduceDctData'*reduceDctData);
-XP = newXP;clear newXP;
+% allEigs = diag(D);
+% fullVariance = sum(allEigs);
+% for zz=5:1:dctDIM
+%     start = dctDIM-zz+1;
+%     eigenvals = allEigs(start:end);
+%     varianceProp = sum(eigenvals)/fullVariance;
+%     if varianceProp >= 0.999
+%         break;
+%     end
+% end
+% q = length(allEigs(start:end));
+% meanEig = mean(allEigs(1:start-1));
+% W = V(:,start:end)*sqrtm(D(start:end,start:end)-meanEig.*eye(q));
+% W = fliplr(W);
+% 
+% x = pinv(W)*pcaData;
+% sparseDctData = x'; % N-by-however-many-dimensions are kept
+% 
+% sparseCov = cov(sparseDctData);
+% lPath
 
 result = struct('ASD',zeros(numIter,2),...
-    'Sparse',zeros(numIter,2));
+    'Sparse',zeros(numIter,2),'PCA',zeros(numIter,2));
 
 [X,Y] = meshgrid(1:DIM(2),1:DIM(1));
 
@@ -86,15 +82,18 @@ maskInds = find(mask);maskSize = length(maskInds);
 
 null = zeros(numIter,1);
 for jj=1:numIter
-    spatFreq = rand*(2/30-2/75)+2/75;
+    spatFreq = rand*(4/75-1/75)+1/75;
     % SIMULATE DATA
     orientation = rand*2*pi;
     currentFilter = filter([spatFreq,orientation,1,normrnd(0,2),normrnd(0,2)]);
     null(jj) = norm(currentFilter);
-    filterOutput = zeros(N,1);
+    filterOutput = zeros(N,1);asdDIM = 100;
+    forASD = zeros(N,asdDIM*asdDIM);
     for kk=1:N
-        temp = fullData(ii,:);temp = reshape(temp,DIM);
-        filterOutput(kk) = sum(temp.*currentFilter);
+        temp = fullData(kk,:);temp = reshape(temp,DIM);
+        smallTemp = imresize(temp,asdDIM(1)/DIM(1));
+        forASD(kk,:) = smallTemp(:);
+        filterOutput(kk) = sum(sum(temp.*currentFilter));
     end
     filterOutput = filterOutput-mean(filterOutput);
     
@@ -103,83 +102,127 @@ for jj=1:numIter
     
     filterOutputNoise = filterOutputNoise-mean(filterOutputNoise);
     
-    filterOutputSig = sigmoid(filterOutput,2,0,1);
+    filterOutputSigmoid = sigmoid(filterOutput,2,0,1);
 
     filterOutputSigmoid = poissrnd(filterOutputSigmoid.*gamrnd(3,1/3,[N,1]));
     meanSigmoid = mean(filterOutputSigmoid);
     filterOutputSigmoid = filterOutputSigmoid-meanSigmoid;
     
     
-    [kestNoise,~,~] = fastASD(fullData,filterOutputNoise,DIM,2);
-    [kestSig,~,~] = fastASD(fullData,filterOutputSigmoid,DIM,2);
-    kestNoise = reshape(kestNoise,DIM);kestSig = reshape(kestSig,DIM);
+    [kestNoise,~,~] = fastASD(forASD,filterOutputNoise,[asdDIM,asdDIM],2);
+    [kestSig,~,~] = fastASD(forASD,filterOutputSigmoid,[asdDIM,asdDIM],2);
+    
+    r1 = corrcoef(forASD*kestNoise,filterOutput);
+    r2 = corrcoef(forASD*kestSig,filterOutput);
     
     %result.ASD(jj,1) = (1/sqrt(prod(DIM)))*norm(kestNoise-currentFilter(:));
-    result.ASD(jj,1) = (1/sqrt(maskSize))*norm(kestNoise(maskInds)-currentFilter(maskInds));
+    result.ASD(jj,1) = r1(1,2);
     
     %result.ASD(jj,2) = (1/sqrt(prod(DIM)))*norm(kestSig-currentFilter(:));
-    result.ASD(jj,2) = (1/sqrt(maskSize))*norm(kestSig(maskInds)-currentFilter(maskInds));
+    result.ASD(jj,2) = r2(1,2);
     
-    cv_result = zeros(length(lPath)+1,2);
-    allInds = 1:N;cv_len = N-round(N/10);
-    for zz=1:length(lPath)+1
-        X = XP(:,:,zz);
-        for yy=1:250
+    
+    %result.Sparse(jj,1) = (1/sqrt(prod(DIM)))*norm(estSparseNoise-currentFilter);
+%     result.Sparse(jj,1) = (1/sqrt(maskSize))*norm(estSparseNoise(maskInds)-currentFilter(maskInds));
+%     
+%     %result.Sparse(jj,2) = (1/sqrt(prod(DIM)))*norm(estSparseSig-currentFilter);
+%     result.Sparse(jj,2) = (1/sqrt(maskSize))*norm(estSparseSig(maskInds)-currentFilter(maskInds));
+    
+    % choose dimensionality for dct images
+    allEigs = diag(D);
+    fullVariance = sum(allEigs);
+    saveVar = 0.7:0.01:0.99;
+    
+    medianCorrs = zeros(length(saveVar),2);
+    count = 1;
+    for yy=saveVar
+        for zz=5:1:dctDIM
+            start = dctDIM-zz+1;
+            eigenvals = allEigs(start:end);
+            varianceProp = sum(eigenvals)/fullVariance;
+            if varianceProp >= yy
+                break;
+            end
+        end
+        q = length(allEigs(start:end));
+        meanEig = mean(allEigs(1:start-1));
+        W = V(:,start:end)*sqrtm(D(start:end,start:end)-meanEig.*eye(q));
+        W = fliplr(W);
+     
+        x = pinv(W)*pcaData;
+        
+        reduceDctData = x'; % N-by-however-many-dimensions are kept
+        
+        allInds = 1:N;cv_iter = 250;
+        tempCorrs = zeros(cv_iter,2);cv_len = round(N*0.9);
+        for zz=1:cv_iter
             inds = randperm(N,cv_len);
             inds = ismember(allInds,inds);
             holdOutInds = ~inds;
-            suffStat = X*reduceDctData(inds,:)';
             
-            tempestSparseNoise = suffStat*filterOutputNoise(inds);
+            estNoise = reduceDctData(inds,:)\filterOutputNoise(inds);
+            r = corrcoef(reduceDctData(holdOutInds,:)*estNoise,filterOutputNoise(holdOutInds));
             
-            r = corrcoef(reduceDctData(holdOutInds,:)*tempestSparseNoise,filterOutputNoise(holdOutInds));
+            tempCorrs(zz,1) = r(1,2);
             
-            cv_result(zz,1) = cv_result(zz,1)+r(1,2);
+            estSig = reduceDctData(inds,:)\filterOutputSigmoid(inds);
+            r = corrcoef(reduceDctData(holdOutInds,:)*estSig,filterOutputSigmoid(holdOutInds));
             
-            tempestSparseSig = suffStat*filterOutputSigmoid(inds);
-            
-            r = corrcoef(max(reduceDctData(holdOutInds,:)*tempestSparseSig+meanSigmoid,0),filterOutputSigmoid(holdOutInds));
-            
-            cv_result(zz,2) = cv_result(zz,2)+r(1,2);
+            tempCorrs(zz,2) = r(1,2);
+        end
+        medianCorrs(count,1) = median(tempCorrs(:,1));
+        medianCorrs(count,2) = median(tempCorrs(:,2));
+        count = count+1;
+    end
+    [~,ind1] = max(medianCorrs(:,1));
+    [~,ind2] = max(medianCorrs(:,2));
+    
+    for zz=5:1:dctDIM
+        start = dctDIM-zz+1;
+        eigenvals = allEigs(start:end);
+        varianceProp = sum(eigenvals)/fullVariance;
+        if varianceProp >= saveVar(min(ind1+1,length(saveVar)))
+            break;
         end
     end
+    q = length(allEigs(start:end));
+    meanEig = mean(allEigs(1:start-1));
+    W = V(:,start:end)*sqrtm(D(start:end,start:end)-meanEig.*eye(q));
+    W = fliplr(W);
     
+    x = pinv(W)*pcaData;
     
-    [~,ind] = max(cv_result(:,1));
-    X = XP(:,:,ind);
-    suffStat = (1/(N-1)).*X*reduceDctData';
-    estSparseNoise = suffStat*filterOutputNoise;
-
-    estSparseNoise = W*estSparseNoise+mu;
-    R = reshape(estSparseNoise,[100,100]);
-    trueShape = zeros(DIM);
-    trueShape(1:100,1:100) = R;
-
-    estSparseNoise = mirt_idctn(trueShape);
-    b = estSparseNoise(:)\currentFilter(:);estSparseNoise = b.*estSparseNoise;
+    reduceDctData = x'; % N-by-however-many-dimensions are kept
+    estNoise = reduceDctData\filterOutputNoise;
     
-    [~,ind] = max(cv_result(:,2));
-    X = XP(:,:,ind);
-    suffStat = (1./(N-1)).*X*reduceDctData';
-    estSparseSig = suffStat*filterOutputSigmoid;
-
-    estSparseSig = W*estSparseSig+mu;
-    R = reshape(estSparseSig,[100,100]);
-    trueShape = zeros(DIM);
-    trueShape(1:100,1:100) = R;
-
-    estSparseSig = mirt_idctn(trueShape);
-    b = estSparseSig(:)\currentFilter(:);estSparseSig = b.*estSparseSig;
+    r1 = corrcoef(reduceDctData*estNoise,filterOutput);
     
-    %result.Sparse(jj,1) = (1/sqrt(prod(DIM)))*norm(estSparseNoise-currentFilter);
-    result.Sparse(jj,1) = (1/sqrt(maskSize))*norm(estSparseNoise(maskInds)-currentFilter(maskInds));
+    for zz=5:1:dctDIM
+        start = dctDIM-zz+1;
+        eigenvals = allEigs(start:end);
+        varianceProp = sum(eigenvals)/fullVariance;
+        if varianceProp >= saveVar(min(ind2+1,length(saveVar)))
+            break;
+        end
+    end
+    q = length(allEigs(start:end));
+    meanEig = mean(allEigs(1:start-1));
+    W = V(:,start:end)*sqrtm(D(start:end,start:end)-meanEig.*eye(q));
+    W = fliplr(W);
     
-    %result.Sparse(jj,2) = (1/sqrt(prod(DIM)))*norm(estSparseSig-currentFilter);
-    result.Sparse(jj,2) = (1/sqrt(maskSize))*norm(estSparseSig(maskInds)-currentFilter(maskInds));
-%         figure;plot(estSparse);hold on;plot(myfilter);
-%         figure;plot(estSparseNoise);hold on;plot(myfilter);
-%         figure;plot(estSparseSig);hold on;plot(myfilter);
-
+    x = pinv(W)*pcaData;
+    
+    reduceDctData = x'; % N-by-however-many-dimensions are kept
+    
+    estSig = reduceDctData\filterOutputSigmoid;
+    
+    r2 = corrcoef(reduceDctData*estSig,filterOutput);
+    
+    %result.PCA(jj,1) = (1/sqrt(prod(DIM)))*norm(estSparseNoise-currentFilter);
+    result.PCA(jj,1) = r1(1,2);
+    
+    %result.PCA(jj,2) = (1/sqrt(prod(DIM)))*norm(estSparseSig-currentFilter);
+    result.PCA(jj,2) = r2(1,2);
 end
 
 save('NatImTests.mat','N','DIM','numIter','result');
@@ -190,11 +233,11 @@ data2 = result.Sparse(:,1);
 figure;histogram(data1);hold on;histogram(data2);
 histogram(null);
 legend('ASD','DCT','Null');title('Gaussian Encoding Model');
-xlabel('RMSE');ylabel('Count');
+xlabel('Correlation Coefficient');ylabel('Count');
 
 data1 = result.ASD(:,2);
 data2 = result.Sparse(:,2);
 figure;histogram(data1);hold on;histogram(data2);
 histogram(null);
 legend('ASD','DCT','Null');title('Poisson-Gamma Encoding Model');
-xlabel('RMSE');ylabel('Count');
+xlabel('Correlation Coefficient');ylabel('Count');
